603d5767498a2e89a463f6880178a829cd7b8b2f
tiny_bear <rebeccawilliams9941@gmail.com>
2023-10-01 14:38:37 +1100

    :tada: Start user interview analysis project!

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title('User Interview Analysis')
+
+    st.write('Upload your user interviews, enter the questions you want to ask about the data and get back a list of quotes that are relevant to the questions you care about.')
+
+if __name__ == '__main__':
+    main()
