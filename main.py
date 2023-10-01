f53d1a2ee7f00e1db89a1c0d5770192feecf2aa4
tiny_bear <rebeccawilliams9941@gmail.com>
2023-10-01 15:16:29 +1100

    :construction: Upload multiple interviews, only new interviews.

--- main.py
+++ main.py
@@ -2,23 +2,40 @@ import streamlit as st
 from example_interviews import example_interview
 
 def main():
-    st.title('User Interview Analysis')
+    if 'interviews' not in st.session_state:
+        st.session_state.interviews = []
 
-    st.write('Upload your user interviews, enter the questions you want to ask about the data and get back a list of quotes that are relevant to the questions you care about.')
+    st.title('User Interview Analysis')
+    st.write('Upload your user interviews, enter the questions you want to ask about the data, and get back a list of quotes that are relevant to the questions you care about.')
 
     use_example_interview = st.checkbox("Try example interview")
 
     if use_example_interview:
-        interview = st.text_area("", example_interview, height=600, max_chars=50000)
+        interview = st.text_area("", example_interview, height=400, max_chars=50000)
     else:
-        interview = st.text_area("Paste in an interview:", '', height=600, max_chars=50000)
+        interview = st.text_area("Paste in an interview:", '', height=400, max_chars=50000)
 
     if st.button('Submit'):
-        st.subheader('Interview:')
+        interviews = st.session_state.get('interviews', [])
+        if interview not in interviews:
+          interviews.append(interview)
+          st.session_state.interviews = interviews
+          st.success("Interview added successfully!")
+        else:
+          st.warning("This interview has already been added.")
+
+    if st.session_state.interviews:
+        st.subheader('Interviews:')
+        interview_options = [f"Interview {i + 1}" for i in range(len(st.session_state.interviews))]
+        selected_index = st.selectbox("Select an interview:", options=interview_options, index=0)
+
+        # Convert "Interview X" to the integer X-1
+        interview_index = int(selected_index.split()[-1]) - 1
+
         st.markdown(
             f"""
             <div style="overflow-y: scroll; height: 300px;">
-                {interview}
+                {st.session_state.interviews[interview_index]}
             </div>
             """,
             unsafe_allow_html=True
